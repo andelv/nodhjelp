6c409c5d31c8ca129f2a15d89b0515e5db6fc1a4
Andreas Elvestad <andelv@live.no>
2014-02-24 20:53:57 +0100

    updated activitybar

--- src/com/example/nodhjelp/MainActivity.java
+++ src/com/example/nodhjelp/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.nodhjelp;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -12,10 +8,13 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
@@ -31,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
 		switch (item.getItemId()) {
 		
 		case R.id.call_emergency:
-			alertMessage();
+			Util.alertMessage(this);
 			return true;
 			
 		case R.id.get_gps_pos:
@@ -44,38 +43,5 @@ public class MainActivity extends ActionBarActivity {
 			return super.onOptionsItemSelected(item);
 			
 		}
-	}
-	
-	public void alertMessage() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Ringe 113?");
-		builder.setMessage("Når du snakker med 113, si da:\n" +
-				"'Jeg oppgir nå grader og desimalminutter'\n\n" +
-				"Er du sikker på at du ønsker å ringe nødnummer: 113?");
-		
-		builder.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-				makeCall("113");
-			}
-		});
-		
-		builder.setNegativeButton("Nei", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-				
-			}
-		});
-		builder.show();
-	}
-	
-	private void makeCall(String phone) {
-		Intent intent = new Intent(Intent.ACTION_CALL);
-		intent.setData(Uri.parse("tel:"+phone));
-		startActivity(intent);
 	}
 }
--- src/com/example/nodhjelp/PositionActivity.java
+++ src/com/example/nodhjelp/PositionActivity.java
@@ -1,20 +1,43 @@
 package com.example.nodhjelp;
 
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class PositionActivity extends Activity {
+public class PositionActivity extends ActionBarActivity implements LocationListener{
 	
-	//float lon, lat, alt;
-	//TextView tv = (TextView) findViewById(R.id.GPS_pos);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		
+		case R.id.call_emergency:
+			Util.alertMessage(this);
+			return true;
+			
+		case R.id.get_gps_pos:
+			Intent intent = new Intent(this, PositionActivity.class);
+			startActivity(intent);
+			
+			
+			
+		default:
+			return super.onOptionsItemSelected(item); }
+			
+		}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.position, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 	
@@ -25,9 +48,49 @@ public class PositionActivity extends Activity {
 		
 		//obtain GPS position
 		LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		LocationListener locationListner = new MyLocationListener(this);
-		
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListner);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 			
 	}
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		TextView tv = (TextView) findViewById(R.id.GPS_pos);
+		float lon,lat,alt, acc, speed;
+		lon = (float) location.getLongitude();
+		lat = (float) location.getLatitude();
+		alt = (float) location.getAltitude();
+		acc = (float) location.getAccuracy();
+		speed =  (float) location.getSpeed();
+		
+		String text = "Din posisjon er: " +
+		"\nLengdegrader = " + lon +
+		"\nBreddegrader = " + lat +
+		"\n\nHøyde = " + alt + 
+		"\nTreffsikkerhet = " + acc + 
+		"\n Fart = " + speed;
+		//String toastText = "Din pos er " +lon +" " +lat;
+		
+		//Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+		tv.setText(text);
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
